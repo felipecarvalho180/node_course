e08bf03546356610f9f1700a7829eb9247241864
Felipe <felipecarvalho180@gmail.com>
2019-10-28 19:44:22 -0300

    Finishs searchs, registers, deletes and updates file in module 4.

--- cli/database.js
+++ cli/database.js
@@ -1,16 +1,17 @@
 
-const { readFileSync } = require('fs');
+const { readFileSync, writeFileSync,  } = require('fs');
 
 class Database {
   constructor() {
     this.FILE_NAME = 'heroes.json';
   }
 
+  // READ
   getData = async () => {
     const file = await readFileSync(this.FILE_NAME, 'utf8');
 
     return JSON.parse(file.toString());
-  }
+  };
 
   list = async id => {
     const data = await this.getData(); 
@@ -18,6 +19,72 @@ class Database {
     const dataFilter = data.filter(item => id ? (item.id === id) : true);
 
     return dataFilter;
+  };
+
+  //CREATE
+  writeFile = async data => {
+    await writeFileSync(this.FILE_NAME, JSON.stringify(data));
+    return true;
+  };
+
+  register = async hero => {
+    const heroes = await this.getData();
+    const id = hero.id <= 2 ? hero.id : Date.now();
+    const completeHero = {
+      id,
+      ...hero,
+    };
+    const finalData = [
+      ...heroes,
+      completeHero,
+    ];
+
+    const result = await this.writeFile(finalData);
+    return result;
+  };
+
+  //DELETE
+  delete = async id => {
+    const heroes = await this.getData();
+
+    const heroIndex = heroes.findIndex(item => item.id === parseInt(id));
+
+    if(heroIndex === -1) throw Error('Hero not found');
+
+    heroes.splice(heroIndex, 1);
+
+    return await this.writeFile(heroes);
+  };
+
+  //UPDATE
+  update = async (hero, heroUpdates) => {
+    const heroes = await this.getData();
+
+    const heroFound = heroes.findIndex(item => item.id === hero.id);
+    
+    if(heroFound === -1) {
+      const updatedHero = {
+        ...hero,
+        ...heroUpdates,
+      }
+
+      return await this.writeFile([
+        updatedHero,
+        ...heroes,
+      ])
+    };
+
+    const updatedHero = {
+      ...heroes[heroFound],
+      ...heroUpdates,
+    };
+    
+    heroes.splice(heroFound, 1);
+
+    return await this.writeFile([
+      updatedHero,
+      ...heroes,
+    ]);
   }
 };
 
--- cli/test.js
+++ cli/test.js
@@ -7,7 +7,17 @@ const DEFAULT_HERO = {
 	id: 1 
 };
 
+const UPDATE_DEFAULT_HERO = {
+	name: 'Iron Man', 
+	power: 'Intelligence', 
+	id: 3,
+}
+
 describe('Heroes manipulation', () => {
+	// before(async () => {
+	// 	await database.register(DEFAULT_HERO);
+	// })
+
 	it('Search heroes with files', async () => {
 		const expected = DEFAULT_HERO;
 		const [ result ] = await database.list(expected.id);
@@ -15,9 +25,36 @@ describe('Heroes manipulation', () => {
 		assert.deepEqual(result, expected);
 	})
 
-	// it('Hero claim with files', async () => {
-	// 	const expected = DEFAULT_HERO;
+	it('Hero registers with files', async () => {
+		const expected = DEFAULT_HERO;
+		await database.register(DEFAULT_HERO);
+		const [actual] = await database.list(DEFAULT_HERO.id);
 
-	// 	assert.ok(null, expected);
-	// });
+		assert.deepEqual(actual, expected);
+	});
+
+	it('Hero removes with file', async () => {
+		const expected = true;
+		const result = await database.delete(DEFAULT_HERO.id);
+
+		assert.deepEqual(result, expected);
+	});
+
+	it('Hero updates with file', async () => {
+		const expected = {
+			...UPDATE_DEFAULT_HERO,
+			name: 'Tony Stark',
+			power: 'Money',
+		};
+
+		const heroUpdated = {
+			name: 'Tony Stark',
+			power: 'Money',
+		}
+
+		await database.update(UPDATE_DEFAULT_HERO, heroUpdated);
+		const [result] = await database.list(UPDATE_DEFAULT_HERO.id);
+
+		assert.deepEqual(result, expected);
+	});
 })
